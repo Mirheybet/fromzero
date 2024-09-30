03cd471be4faae29fec2dc1312a10a24cf9a3e82
root <mirheybet.hesenov@gmail.com>
2024-09-30 23:16:55 +0000

    Book&Library System - CRUD

--- python/OOP/2-book.py
+++ python/OOP/2-book.py
@@ -36,6 +36,9 @@ class Book:
             self.__borrow = borrow
         else:
             raise ValueError("BORROW must be str")
+    def __str__(self):
+        borrow_status = "Borrow" if self.isborrow else "Available"
+        return f'{self.title} by {self.author} ({borrow_status})'
 
 class Library:
     def __init__(self,books_db):
@@ -46,10 +49,36 @@ class Library:
         if (len(book_list)==0):
             print("Empty")
         for index in range(len(self.books_db)):
-            if ((index+1) == id):
+            if (index == (id-1)):
                 return self.books_db[index]
-            
 
+    def create(self,title,author,isborrow):
+        new_book = Book(title,author,isborrow)
+        self.books_db.append(new_book)
+        print("Book successful added....",end="\n")
+        print(f"Book '{new_book.title}' by {new_book.author} has been added to the library.\n")
+        print(f"Book status is {new_book.isborrow}!")
+
+    def update(self,n_nth_book,n_title,n_author,n_isborrow):
+        if(isinstance(n_nth_book,int)):
+            if (n_nth_book < 0 or n_nth_book > len(self.books_db)):
+                raise IndexError(f"There are not {n_nth_book} ID book in library :(")
+        
+            book_upd = self.books_db[n_nth_book - 1]
+        
+            if n_title:
+                book_upd.title = n_title
+            if n_author:
+                book_upd.author = n_author
+            if n_isborrow:
+                book_upd.isborrow = n_isborrow
+        else:
+            raise TypeError("Book id must be integer")
+    def delete(self,id):
+         if(isinstance(id,int)):
+            if (id < 0 or id > len(self.books_db)):
+                raise IndexError(f"There are not {n_nth_book} ID book in library :(")
+            process = self.books_db.pop(id-1)
 
 b1=Book("Development","Microsoft",False)
 b2=Book("Cloud","Amazon",True)
@@ -59,9 +88,55 @@ book_list.append(b1)
 book_list.append(b2)
 book_list.append(b3)
 
-for one_book in book_list:
-    print(one_book.title, " - ", one_book.author," - ",one_book.isborrow,end="\n")
+for id,one_book in enumerate(book_list,start=1):
+    print(id,". ",one_book.title, " - ", one_book.author," - ",one_book.isborrow,end="\n")
 
-print("<<CRUD>>")
+print("\n<<CRUD starting....>>\n")
 b1crud=Library(book_list)
-print(b1crud.read(1))
+
+#READ-for nth book
+print("\n(C)Read(UD)")
+print("(C)Read(UD) STARTING....")
+nth_book_liblist = int(input("N-th book index,pls write:"))
+print(f"\n{nth_book_liblist} -th book:\n")
+print(b1crud.read(nth_book_liblist))
+print("(C)Read(UD) COMPLETED!\n")
+
+#Create
+print("\nCreate(RUD) STARTING....")
+title = input("Enter book title: ")
+author = input("Enter book author: ")
+isborrow = input("Is the book borrowed? (yes/no): ").lower() == "yes"
+b1crud.create(title, author, isborrow)
+print("After UPDATE")
+for id,i in enumerate(book_list,start=1):
+    print(f"{id}. {i}")
+print("\nCreate(RUD) COMPLETED!\n")
+
+#Update
+print("\n(CR)Update(D) STARTING....\n")
+upd_id = int(input("Enter book id(integer): "))
+upd_title = input("Enter book title: ")
+upd_author = input("Enter book author: ")
+upd_isborrow = input("Is the book borrowed? (yes/no): ").lower() == "yes"
+b1crud.update(upd_id,upd_title,upd_author,upd_isborrow)
+print("After UPDATE")
+for id,i in enumerate(book_list,start=1):
+    print(f"{id}. {i}")
+print("\n(CR)Update(D) COMPLETED....\n")
+
+#Delete
+print("\n(CRU)Delete STARTING....\n")
+del_id = int(input("Enter book id(integer): "))
+a_or_dagree = input("Do you agree (yes/no): ")
+if (a_or_dagree.lower() == "yes"):
+    b1crud.delete(del_id)
+    print("After UPDATE")
+    for id,i in enumerate(book_list,start=1):
+        print(f"{id}. {i}")
+    print("\n(CR)Update(D) COMPLETED....\n")
+else:
+    print("DELETE cancelled!")
+    
+
+print("\n<<CRUD completed....>>\n")
